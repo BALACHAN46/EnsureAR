c5fe0f91d6f2eb2e33ab2aac7a1175b4fa0a2513
BalaChandiran <balachandiranv@vaanamtech.com>
2026-08-10 23:54:55 +0530

    chnage the fornttend url to ngrok

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -12,7 +12,9 @@ import { API_BASE_URL } from './config';
 import { getToken, clearAuth } from '../utils/auth';
 
 async function request(path, { method = 'GET', body, isForm = false, auth = true } = {}) {
-  const headers = {};
+  const headers = {
+    'ngrok-skip-browser-warning': 'true'
+  };
   if (!isForm && body !== undefined) headers['Content-Type'] = 'application/json';
   let tokenAttached = false;
   if (auth) {
